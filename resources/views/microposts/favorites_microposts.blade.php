056ba2cfd96867626261797e35bbde47074ba3a8
reita.nakatani <himono55@gmail.com>
2019-04-07 01:45:16 +0000

    correction_followers

---
+++ resources/views/microposts/favorites_microposts.blade.php
@@ -0,0 +1,28 @@
+@if(counts($user) > 0)
+    @foreach($favorite as $favorites)
+        <li class="media mb-3">
+            <img class="mr-2 rounded" src="{{Gravatar::src($user->email,50) }}" alt="">
+            <div class="media-body">
+                <div>
+                    {!! link_to_route('users.show',$micropost->user->name,['id'=>$micropost->user->id]) !!}<span class="text-muted">posted at {{ $micropost->created_at }}</span>
+                </div>
+                <div>
+                    <p class="mb-0">{!! nl2br(e($micropost->content)) !!}</p>
+                    
+                </div>
+                <div>
+                    @if(Auth::id()==$micropost->user_id)
+                        {!! Form::open(['route' => ['microposts.destroy', $micropost->id], 'method' => 'delete']) !!}
+                            {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-sm']) !!}
+                        {!! Form::close() !!}
+                    @endif
+                </div>
+                <div>
+                    @include('user_favorite.favorite_button',['favorites'=>$micropost])
+                </div>
+            </div>
+        </li>
+    @endforeach
+</ul>
+{{$microposts->render('pagination::bootstrap-4') }}
+    
